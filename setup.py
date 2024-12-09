1ef8424190f4e9314fe729b3ff2fcccea544bc32
Vishnuu011 <vishnurrajeev@gmail.com>
2024-12-09 17:28:41 +0530

    update template and setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ChurnPrediction",
+    version="0.0.0",
+    author="vishnu",
+    author_email="vishnurrajeev@gmail.com",
+    packages=find_packages()
+
+)
